8d75500bea8332fdb387f28a0d8d339e6b7d4e8d
Sushant Kumar <Sushant.Kumar6@ey.com>
2024-07-16 21:09:07 +0530

    Basic Setup and two sum problem.

---
+++ CSharpCodingQuestions/CSharpCodingQuestions/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpCodingQuestions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [2, 11, 7, 15];
+        int target = 9;
+        var result = TwoSumProblem.TwoSumBruteForce(nums, target);
+        Console.WriteLine($"[{result[0]}, {result[1]}]");
+    }
+}
