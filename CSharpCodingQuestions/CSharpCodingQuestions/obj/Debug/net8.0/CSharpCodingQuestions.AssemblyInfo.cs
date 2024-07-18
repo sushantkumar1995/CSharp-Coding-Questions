8d484b872274a9a712f5b3a297387b3f3f319da9
Sushant Kumar <sushantkumar1995@gmail.com>
2024-07-18 16:21:08 +0530

    resolve changes

--- CSharpCodingQuestions/CSharpCodingQuestions/obj/Debug/net8.0/CSharpCodingQuestions.AssemblyInfo.cs
+++ CSharpCodingQuestions/CSharpCodingQuestions/obj/Debug/net8.0/CSharpCodingQuestions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCodingQuestions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2050856afc366a6ea1d022f394fae94b63b6739c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d0da8e3e7d3a3c7cda1d47b2ae17938648b700")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCodingQuestions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCodingQuestions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
